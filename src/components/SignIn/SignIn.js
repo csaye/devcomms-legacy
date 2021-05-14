18ef27a430205aaacccb15252784c2ba899e6f5e
Cooper Saye <coopersaye@gmail.com>
2021-05-14 15:56:37 -0600

    Create SignIn component.

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+
+import './SignIn.css';
+
+function SignIn() {
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  return (
+    <div className="SignIn">
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
+  );
+}
+
+export default SignIn;
