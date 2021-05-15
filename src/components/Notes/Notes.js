512484c128326f554b47978aefd31bf0954abc88
Cooper Saye <coopersaye@gmail.com>
2021-05-15 11:42:06 -0600

    Create Notes component.

---
+++ src/components/Notes/Notes.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+
+import firebase from 'firebase/app';
+
+import './Notes.css';
+
+function Notes() {
+  const [text, setText] = useState('');
+  const [placeholder, setPlaceholder] = useState('Loading notes...');
+
+  const noteRef = firebase.firestore().collection('notes').doc('note');
+
+  // gets text from firebase document
+  async function getText() {
+    await noteRef.get().then(doc => {
+      const docData = doc.data();
+      setText(docData.text);
+      setPlaceholder('');
+    });
+  }
+
+  // updates firebase document text
+  async function updateText(newText) {
+    await noteRef.update({
+      text: newText
+    });
+  }
+
+  // get text on start
+  useEffect(() => {
+    getText();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div className="Notes">
+      <h1>Notes</h1>
+      <textarea
+        placeholder={placeholder}
+        value={text}
+        className="flex-grow"
+        onChange={e => {
+          const val = e.target.value;
+          setText(val);
+          updateText(val);
+        }}
+      />
+    </div>
+  );
+}
+
+export default Notes;
