0739376b51be2bc44d2a74ce49ad418354264075
Cooper Saye <coopersaye@gmail.com>
2021-05-16 14:50:50 -0600

    Implement sketch colors.

--- src/components/CanvasSketch/CanvasSketch.js
+++ src/components/CanvasSketch/CanvasSketch.js
@@ -1,6 +1,9 @@
 import React, { useRef, useEffect, useState } from 'react';
 
 import BrushIcon from '@material-ui/icons/Brush';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FormatColorResetIcon from '@material-ui/icons/FormatColorReset';
 
 import firebase from 'firebase/app';
 
@@ -9,7 +12,6 @@ import './CanvasSketch.css';
 const width = 256;
 const height = 256;
 
-let lineColor = 'black';
 let lineWidth = 2;
 let prevX = 0, prevY = 0, currX = 0, currY = 0;
 let drawing = false;
@@ -17,15 +19,16 @@ let drawing = false;
 let canvas;
 let ctx;
 
-function CanvasSketch() {
+const lineColors = ['red', 'orange', 'yellow', 'green', 'blue', 'black'];
 
+function CanvasSketch() {
+  const [lineColor, setLineColor] = useState('black');
   const [loaded, setLoaded] = useState(false);
   const canvasRef = useRef();
   const sketchRef = firebase.firestore().collection('sketches').doc('sketch');
 
   // updates sketch in firebase
   async function updateSketch() {
-    console.log('updating sketch');
     const url = canvas.toDataURL();
     await sketchRef.update({
       data: url
@@ -64,7 +67,13 @@ function CanvasSketch() {
 
   // clears canvas
   function clearCanvas() {
-    ctx.clearRect(0, 0, width, height);
+
+    // fill with white
+    ctx.beginPath();
+    ctx.rect(0, 0, width, height);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.closePath();
 
     // update sketch in firebase
     updateSketch();
@@ -75,7 +84,7 @@ function CanvasSketch() {
     const downloadLink = document.createElement('a');
     downloadLink.setAttribute('download', 'sketch.png');
     canvas.toBlob(blob => {
-      let url = URL.createObjectURL(blob);
+      const url = URL.createObjectURL(blob);
       downloadLink.setAttribute('href', url);
       downloadLink.click();
     });
@@ -86,6 +95,7 @@ function CanvasSketch() {
     canvas = canvasRef.current;
     ctx = canvas.getContext('2d');
 
+    // get sketch from firebase and load
     sketchRef.get().then(doc => {
       const docData = doc.data();
       const url = docData.data;
@@ -116,12 +126,39 @@ function CanvasSketch() {
       />
       {
         loaded ?
-        <div>
-          <button onClick={clearCanvas}>Clear</button>
-          <button onClick={downloadCanvas} ref={downloadRef}>
-            Download
-          </button>
-        </div> :
+        <>
+          <div className="special-buttons">
+            <button
+              onClick={() => setLineColor("white")}
+              style={{"background": "white"}}
+              className={
+                lineColor === "white" ?
+                "color-button white-selected" :
+                "color-button"
+              }
+            />
+          </div>
+          <div className="color-buttons">
+            {
+              lineColors.map((color, i) =>
+                <button
+                  key={`colorbutton-${i}`}
+                  onClick={() => setLineColor(color)}
+                  style={{"background": color}}
+                  className={
+                    lineColor === color ?
+                    "color-button selected" :
+                    "color-button"
+                  }
+                />
+              )
+            }
+          </div>
+          <div>
+            <button className="canvas-button" onClick={clearCanvas}><DeleteIcon /></button>
+            <button className="canvas-button" onClick={downloadCanvas}><GetAppIcon /></button>
+          </div>
+        </> :
         <p>Loading sketch...</p>
       }
     </div>
