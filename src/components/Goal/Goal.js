07c71763a2dbac7da9051398086cf03d61f1d457
Cooper Saye <coopersaye@gmail.com>
2021-05-16 07:33:41 -0600

    Implement goal countdown.

--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -1,12 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
 import CheckIcon from '@material-ui/icons/Check';
 
 import firebase from 'firebase/app';
 
 import './Goal.css';
 
+const SEC_MS = 1000;
+const MIN_MS = SEC_MS * 60;
+const HOUR_MS = MIN_MS * 60;
+const DAY_MS = HOUR_MS * 24;
+
 function Goal(props) {
   const { text, id, endAt } = props.data;
   const endDate = endAt.toDate();
+
+  const [timeLeft, setTimeLeft] = useState(endDate - new Date());
 
   const goalsRef = firebase.firestore().collection('goals');
 
@@ -15,10 +24,25 @@ function Goal(props) {
     await goalsRef.doc(id).delete();
   }
 
+  useEffect(() => {
+    // update time left every tenth of a second
+    setTimeLeft(endDate - new Date());
+    const interval = setInterval(() => {
+      setTimeLeft(endDate - new Date());
+    }, 100);
+    return () => clearInterval(interval);
+  }, [endAt]);
+
   return (
     <div className="Goal">
       <p>{text}</p>
       <p>{`${endDate.toLocaleDateString()}, ${endDate.toLocaleTimeString()}`}</p>
+      <p className="time-left">
+        {Math.floor(timeLeft / DAY_MS)}<span className="time-icon">d</span>
+        {Math.floor(timeLeft % DAY_MS / HOUR_MS)}<span className="time-icon">h</span>
+        {Math.floor(timeLeft % DAY_MS % HOUR_MS / MIN_MS)}<span className="time-icon">m</span>
+        {Math.floor(timeLeft % DAY_MS % HOUR_MS % MIN_MS / SEC_MS)}<span className="time-icon">s</span>
+      </p>
       <button onClick={deleteGoal}>
         <CheckIcon />
       </button>
