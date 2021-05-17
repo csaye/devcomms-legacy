43ff314fe7cb2c7e30f82c63522bc48d3fa646a4
Cooper Saye <coopersaye@gmail.com>
2021-05-17 16:05:20 -0600

    Implement commit list.

--- src/components/GitHub/GitHub.js
+++ src/components/GitHub/GitHub.js
@@ -12,6 +12,7 @@ function GitHub() {
   const [user, setUser] = useState('');
   const [repo, setRepo] = useState('');
   const [repoJson, setRepoJson] = useState(undefined);
+  const [commitsJson, setCommitsJson] = useState(undefined);
 
   const repoRef = firebase.firestore().collection('repos').doc('repo');
   const [repoData] = useDocumentData(repoRef);
@@ -25,14 +26,29 @@ function GitHub() {
     });
   }
 
+  // gets data for current repository
   async function getRepoData() {
+
+    // verify repo data and get url
     if (!repoData || !repoData.user || !repoData.repo) return;
     const url = `https://api.github.com/repos/${repoData.user}/${repoData.repo}`;
+
+    // get repo data
     await fetch(url).then(response => {
       if (response.ok) return response.json();
       else throw new Error('GitHub request failed!');
     }).then(json => {
+      console.log(json);
       setRepoJson(json);
+
+      // get commit data
+      fetch(`${url}/commits?per_page=10`).then(cResponse => {
+        if (cResponse.ok) return cResponse.json();
+        else throw new Error('GitHub request failed!');
+      }).then(cJson => {
+        console.log(cJson);
+        setCommitsJson(cJson);
+      })
     });
   }
 
@@ -71,13 +87,40 @@ function GitHub() {
           <a href={repoJson.html_url} target="_blank" rel="noreferrer">
             <h1>{repoJson.full_name}</h1>
           </a>
-          <p>Updated at
+          <p>Last updated:<br />
             {' '}{new Date(repoJson.updated_at).toDateString()},
             {' '}{new Date(repoJson.updated_at).toLocaleTimeString()}
           </p>
           <p>Written in {repoJson.language}</p>
         </div> :
         <p>Loading repo...</p>
+      }
+      {
+        commitsJson ?
+        <div>
+          <a href={repoJson.html_url + '/commits'} target="_blank" rel="noreferrer">
+            <h1>commits</h1>
+          </a>
+          {
+            commitsJson.length > 0 ?
+            commitsJson.map((c, i) =>
+              <div key={`commit-${i}`}>
+                <p>
+                  <a href={c.author.html_url} target="_blank" rel="noreferrer">
+                    @{c.author.login}
+                  </a>
+                  {' - '}
+                  <a href={c.html_url} target="_blank" rel="noreferrer">
+                    {c.commit.message}
+                  </a>
+                  {' (' + new Date(c.commit.committer.date).toLocaleDateString() + ')'}
+                </p>
+              </div>
+            ) :
+            <p>No commits yet</p>
+          }
+        </div> :
+        <p>Loading commits...</p>
       }
     </div>
   );
