bb9e64a6e07e3ddd58e3d57eab93c7047dd0f6f2
Cooper Saye <coopersaye@gmail.com>
2021-05-17 16:44:31 -0600

    Implement GitHub error messages.

--- src/components/GitHub/GitHub.js
+++ src/components/GitHub/GitHub.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
+import Commit from '../Commit/Commit.js';
+
 import GitHubIcon from '@material-ui/icons/GitHub';
 import SearchIcon from '@material-ui/icons/Search';
+
+import { githubToken } from '../../util/githubToken.js';
 
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 import firebase from 'firebase/app';
@@ -13,6 +17,7 @@ function GitHub() {
   const [repo, setRepo] = useState('');
   const [repoJson, setRepoJson] = useState(undefined);
   const [commitsJson, setCommitsJson] = useState(undefined);
+  const [error, setError] = useState('');
 
   const repoRef = firebase.firestore().collection('repos').doc('repo');
   const [repoData] = useDocumentData(repoRef);
@@ -31,22 +36,34 @@ function GitHub() {
 
     // verify repo data and get url
     if (!repoData || !repoData.user || !repoData.repo) return;
-    const url = `https://api.github.com/repos/${repoData.user}/${repoData.repo}`;
+    setUser(repoData.user);
+    setRepo(repoData.repo);
+    const repoName = `${repoData.user}/${repoData.repo}`;
+    const url = `https://api.github.com/repos/${repoName}`;
+    const options = {
+      headers: {
+        'Authorization': githubToken
+      }
+    };
 
     // get repo data
-    await fetch(url).then(response => {
+    await fetch(url, options).then(response => {
       if (response.ok) return response.json();
-      else throw new Error('GitHub request failed!');
     }).then(json => {
-      console.log(json);
+      if (!json) {
+        setError(repoName + ' is not a valid repository.');
+        return;
+      }
       setRepoJson(json);
 
       // get commit data
-      fetch(`${url}/commits?per_page=10`).then(cResponse => {
+      fetch(`${url}/commits?per_page=10`, options).then(cResponse => {
         if (cResponse.ok) return cResponse.json();
-        else throw new Error('GitHub request failed!');
       }).then(cJson => {
-        console.log(cJson);
+        if (!cJson) {
+          setError(repoName + ' is not a valid repository.');
+          return;
+        }
         setCommitsJson(cJson);
       })
     });
@@ -82,45 +99,40 @@ function GitHub() {
         </div>
       </form>
       {
-        repoJson ?
-        <div>
-          <a href={repoJson.html_url} target="_blank" rel="noreferrer">
-            <h1>{repoJson.full_name}</h1>
-          </a>
-          <p>Last updated:<br />
-            {' '}{new Date(repoJson.updated_at).toDateString()},
-            {' '}{new Date(repoJson.updated_at).toLocaleTimeString()}
-          </p>
-          <p>Written in {repoJson.language}</p>
-        </div> :
-        <p>Loading repo...</p>
-      }
-      {
-        commitsJson ?
-        <div>
-          <a href={repoJson.html_url + '/commits'} target="_blank" rel="noreferrer">
-            <h1>commits</h1>
-          </a>
+        error ?
+        <p className="error-text">{error}</p> :
+        <>
           {
-            commitsJson.length > 0 ?
-            commitsJson.map((c, i) =>
-              <div key={`commit-${i}`}>
-                <p>
-                  <a href={c.author.html_url} target="_blank" rel="noreferrer">
-                    @{c.author.login}
-                  </a>
-                  {' - '}
-                  <a href={c.html_url} target="_blank" rel="noreferrer">
-                    {c.commit.message}
-                  </a>
-                  {' (' + new Date(c.commit.committer.date).toLocaleDateString() + ')'}
-                </p>
+            (repoJson && commitsJson) ?
+            <>
+              <div>
+                <a href={repoJson.html_url} target="_blank" rel="noreferrer">
+                  <h1>{repoJson.full_name}</h1>
+                </a>
+                <div className="repo-data">
+                  <p>Last updated:<br />
+                    {' '}{new Date(repoJson.updated_at).toDateString()},
+                    {' '}{new Date(repoJson.updated_at).toLocaleTimeString()}
+                  </p>
+                  <p>Written in {repoJson.language}</p>
+                </div>
               </div>
-            ) :
-            <p>No commits yet</p>
+              <div>
+                <a href={repoJson.html_url + '/commits'} target="_blank" rel="noreferrer">
+                  <h1>commits</h1>
+                </a>
+                {
+                  commitsJson.length > 0 ?
+                  commitsJson.map((c, i) =>
+                    <Commit key={`commit-${i}`} data={c} />
+                  ) :
+                  <p>No commits yet</p>
+                }
+              </div>
+            </> :
+            <p>Loading repo data...</p>
           }
-        </div> :
-        <p>Loading commits...</p>
+        </>
       }
     </div>
   );
