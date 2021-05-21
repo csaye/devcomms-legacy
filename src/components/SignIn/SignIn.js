23cc4e1ecb3f78606ebda2159e44e3ed41e3b12e
Cooper Saye <coopersaye@gmail.com>
2021-05-21 08:58:26 -0600

    Implement email and password sign in.

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,12 +1,36 @@
+import React, { useState } from 'react';
+
 import firebase from 'firebase/app';
 
 import logo from '../../img/logo.png';
 import './SignIn.css';
 
 function SignIn() {
-  function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // attempts to sign in user
+  async function signIn(e) {
+    e.preventDefault();
+    setError('');
+    // try to sign in with email and password
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+    // handle sign in error
+    } catch(e) {
+      if (e.code === 'auth/invalid-email') {
+        setError('Invalid email address.');
+      } else if (e.code === 'auth/user-not-found') {
+        setError('Unknown email address.');
+      } else if (e.code === 'auth/wrong-password') {
+        setError('Incorrect password. Please try again.');
+      } else if (e.code === 'auth/too-many-requests') {
+        setError('Too many sign in requests. Please try again later.')
+      } else {
+        setError(e.message);
+      }
+    }
   }
 
   return (
@@ -15,7 +39,23 @@ function SignIn() {
         <img src={logo} alt="logo" />
         <h1>DevComms</h1>
         <hr />
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <form onSubmit={signIn}>
+          <input
+            placeholder="email"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+          <input
+            placeholder="password"
+            type="password"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+          <button type="submit">Sign In</button>
+        </form>
       </div>
     </div>
   );
