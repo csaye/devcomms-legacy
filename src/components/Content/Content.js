dc4659ad6d9ab89eae3dc28522d1e1b47b429a75
Cooper Saye <coopersaye@gmail.com>
2021-06-09 23:31:58 -0700

    Create Content component.

---
+++ src/components/Content/Content.js
@@ -0,0 +1,22 @@
+// import Chat from '../Chat/Chat.js';
+// import Notes from '../Notes/Notes.js';
+// import Goals from '../Goals/Goals.js';
+// import Sketch from '../Sketch/Sketch.js';
+// import Todos from '../Todos/Todos.js';
+
+// import ListIcon from '@material-ui/icons/List';
+// import BrushIcon from '@material-ui/icons/Brush';
+// import LibraryAddCheckIcon from '@material-ui/icons/LibraryAddCheck';
+// import DescriptionIcon from '@material-ui/icons/Description';
+
+import './Content.css';
+
+function Content(props) {
+  return (
+    <div className="Content">
+      <p>Content</p>
+    </div>
+  );
+}
+
+export default Content;
