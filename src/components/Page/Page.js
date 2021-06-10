4153b9fe1c9429bc5c04f37da05b1d2a8e6bbf68
Cooper Saye <coopersaye@gmail.com>
2021-06-09 23:42:35 -0700

    Clean up Page component.

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,48 +1,24 @@
-import React, { useState } from 'react';
-
 import Groups from '../Groups/Groups.js';
-import Chat from '../Chat/Chat.js';
 import Header from '../Header/Header.js';
-import Notes from '../Notes/Notes.js';
-import Goals from '../Goals/Goals.js';
-import Sketch from '../Sketch/Sketch.js';
-import Todos from '../Todos/Todos.js';
-
-import ListIcon from '@material-ui/icons/List';
-import BrushIcon from '@material-ui/icons/Brush';
-import LibraryAddCheckIcon from '@material-ui/icons/LibraryAddCheck';
-import DescriptionIcon from '@material-ui/icons/Description';
+import Channels from '../Channels/Channels.js';
+import Content from '../Content/Content.js';
 
 import './Page.css';
 
 function Page(props) {
-  const [widget, setWidget] = useState('');
-
-  function getWidget() {
-    switch (widget) {
-      case 'notes': return <Notes group={props.group} />
-      case 'goals': return <Goals group={props.group} />
-      case 'sketch': return <Sketch group={props.group} />
-      case 'todos': return <Todos group={props.group} />
-      default: return <p className="select-text">Select a widget above.</p>
-    }
-  }
-
   return (
     <div className="Page">
       <Header group={props.group} username={props.username} />
       <div className="page-content">
         <Groups />
-        <Chat group={props.group} username={props.username} />
-        <div className="select-widget">
-          <div className="widget-header">
-            <button className={widget === 'todos' ? 'clean-btn selected' : 'clean-btn'} onClick={() => setWidget('todos')}><ListIcon /></button>
-            <button className={widget === 'sketch' ? 'clean-btn selected' : 'clean-btn'} onClick={() => setWidget('sketch')}><BrushIcon /></button>
-            <button className={widget === 'goals' ? 'clean-btn selected' : 'clean-btn'} onClick={() => setWidget('goals')}><LibraryAddCheckIcon /></button>
-            <button className={widget === 'notes' ? 'clean-btn selected' : 'clean-btn'} onClick={() => setWidget('notes')}><DescriptionIcon /></button>
-          </div>
-          <div className="widget">{getWidget()}</div>
-        </div>
+        {
+          props.group ?
+          <>
+            <Channels group={props.group} />
+            <Content />
+          </> :
+          <p>No group selected</p>
+        }
       </div>
     </div>
   );
