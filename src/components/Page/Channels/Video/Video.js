77d42aef09503d923363388de3c4c288ebd2a20e
Cooper Saye <coopersaye@gmail.com>
2021-06-13 22:00:57 -0700

    Implement peer connection.

--- src/components/Page/Channels/Video/Video.js
+++ src/components/Page/Channels/Video/Video.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Peer from 'peerjs';
 
 import VideocamIcon from '@material-ui/icons/Videocam';
@@ -15,6 +15,10 @@ function Video(props) {
   const [streaming, setStreaming] = useState(false);
 
   const videoGridRef = useRef();
+
+  // get channel doc reference
+  const groupDoc = firebase.firestore().collection('groups').doc(props.group);
+  const channelDoc = groupDoc.collection('channels').doc(props.channel);
 
   // creates and returns a video object streaming from given stream
   function addVideoStream(stream) {
@@ -60,14 +64,37 @@ function Video(props) {
     setStreaming(false);
   }
 
+  function connectToPeer(peerId) {
+    const call = localPeer.call(peerId, localStream);
+    const video = document.createElement('video');
+    call.on('stream', remoteStream => {
+      addVideoStream(video, remoteStream);
+    });
+    call.on('close', () => video.remove());
+  }
+
+  async function joinCall() {
+    const docData = (await channelDoc.get()).data();
+    docData.ids.forEach(peerId => connectToPeer(peerId));
+    await channelDoc.update({
+      ids: firebase.firestore.FieldValue.arrayUnion(localPeer.id)
+    });
+    setCalling(true);
+  }
+
   return (
     <div className="Video">
       <h1><VideocamIcon /> Video</h1>
       <div className="video-grid" ref={videoGridRef}></div>
       {
-        streaming ?
-        <button onClick={stopWebcam}>Stop Webcam</button> :
-        <button onClick={startWebcam}>Start Webcam</button>
+        !streaming ?
+        <button onClick={startVideo}>Start Video</button> :
+        !calling ?
+        <>
+          <button onClick={joinCall}>Join Call</button>
+          <button onClick={stopVideo}>Stop Video</button>
+        </> :
+        <button onClick={leaveCall}>Leave Call</button>
       }
     </div>
   );
