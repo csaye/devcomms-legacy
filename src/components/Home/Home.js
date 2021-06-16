66f1a95d784db65d6c5db92927770f85c23cec9b
Cooper Saye <coopersaye@gmail.com>
2021-06-16 11:23:46 -0700

    Pull cached groups and channels.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
 import Page from '../Page/Page.js';
 import Loading from '../Loading/Loading.js';
@@ -9,11 +10,34 @@ import { useDocumentData } from 'react-firebase-hooks/firestore';
 import './Home.css';
 
 function Home() {
+  // retrieve user data
   const uid = firebase.auth().currentUser.uid;
   const userDoc = firebase.firestore().collection('users').doc(uid);
   const [userData] = useDocumentData(userDoc);
 
   const { groupId, channelId } = useParams();
+  const history = useHistory();
+
+  // pulls cached group and channel data
+  function pullCached() {
+    if (!userData) return;
+    // if no group id, pull from firebase
+    if (!groupId) {
+      if (userData.group) {
+        history.push(`/home/${userData.group}`);
+      }
+    // if group id, pull channel from firebase
+    } else {
+      if (userData.channels[groupId]) {
+        history.push(`/home/${groupId}/${userData.channels[groupId]}`);
+      }
+    }
+  }
+
+  // pull cached group and channel data on start
+  useEffect(() => {
+    pullCached();
+  }, [userData, groupId]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="Home">
