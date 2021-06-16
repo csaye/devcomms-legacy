8a64e7006e7454f5f0ef31f7dc3b2152f70ca9dd
Cooper Saye <coopersaye@gmail.com>
2021-06-15 20:56:59 -0700

    Prepare for deploy.

---
+++ src/util/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCKayJE2th9hAZKwIqzr4iIAsODco8HaXE",
+  authDomain: "dev-comms.firebaseapp.com",
+  projectId: "dev-comms",
+  storageBucket: "dev-comms.appspot.com",
+  messagingSenderId: "133639903558",
+  appId: "1:133639903558:web:c801b4f5d6b4fa2ddfad47"
+};
