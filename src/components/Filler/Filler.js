8433b3ae83b000a463aae26a95aa26d056e80659
Cooper Saye <coopersaye@gmail.com>
2021-06-18 22:54:59 -0700

    Create loading filler.

--- src/components/Filler/Filler.js
+++ src/components/Filler/Filler.js
@@ -1,11 +1,21 @@
 import nodata from '../../img/nodata.svg';
+import loading from '../../img/loading.svg';
 import './Filler.css';
 
 function Filler(props) {
+  // retrieves image for given type
+  function getImage(type) {
+    switch (type) {
+      case 'nodata': return nodata;
+      case 'loading': return loading;
+      default: return null;
+    }
+  }
+
   return (
     <div className="Filler">
       <div>
-        <img src={nodata} alt="nodata" />
+        <img src={getImage(props.type)} alt={props.type} />
         <h1>{props.message}</h1>
       </div>
     </div>
