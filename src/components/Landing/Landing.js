d2999a8ca3776651183fd519b2a086c6c43cb3ea
Cooper Saye <coopersaye@gmail.com>
2021-06-21 11:46:39 -0700

    Improve landing page styling.

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,5 +1,9 @@
 import GitHubIcon from '@material-ui/icons/GitHub';
+import GroupIcon from '@material-ui/icons/Group';
+import AddIcon from '@material-ui/icons/Add';
 import { getIcon } from '../Page/Channels/Channels.js';
+
+import firebase from 'firebase/app';
 
 import logo from '../../img/logo.png';
 import './Landing.css';
@@ -15,7 +19,15 @@ function Landing() {
         <div className="description">
           <b>Devcomms is an open source, developer-focused team communication and coordination tool aiming to simplify and streamline development.</b>
           <div className="line-margin flex-align icon-list">
-            {getIcon('text')}{getIcon('sketch')}{getIcon('notes')}{getIcon('todos')}{getIcon('goals')}{getIcon('audio')}{getIcon('video')}
+            <GroupIcon />
+            {getIcon('text')}
+            {getIcon('sketch')}
+            {getIcon('audio')}
+            {getIcon('video')}
+            {getIcon('notes')}
+            {getIcon('todos')}
+            {getIcon('goals')}
+            <AddIcon />
           </div>
           Instead of having one tool for chatting and calling, one tool for arranging notes, and one tool for setting goals and to-dos, you can do all of that and more with Devcomms. No longer will you have to struggle to coordinate across several apps.<br />
         </div>
@@ -38,9 +50,14 @@ function Landing() {
         </div>
         <hr />
         <div className="links">
-          <a href="/signin">Sign in</a>
-          {' | '}
-          <a href="/signup">Sign up</a>
+          {
+            firebase.auth().currentUser ?
+            <a href="/home">Open Devcomms</a> :
+            <>
+              <a href="/signin">Sign in</a>
+              <a href="/signup">Sign up</a>
+            </>
+          }
         </div>
       </div>
     </div>
